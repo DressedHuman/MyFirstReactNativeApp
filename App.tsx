f7374b72dc78d54dcf6c8dd98b66646f768c2817
DressedHuman <dressed.human@gmail.com>
2024-04-21 15:01:38 +0600

    wrote my firstever react native class component

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const App = () => {
-  return (
-    <View>
-      <Text>Hello, world!</Text>
-      <Text>This is my first react native stateless component!</Text>
-    </View>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello, world!</Text>
+        <Text>This is my first-ever react native class component!</Text>
+      </View>
+    );
+  }
+}
 
 export default App;
